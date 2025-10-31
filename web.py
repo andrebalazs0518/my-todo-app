057368f060f748d653c4aebfc4e96d765afacc23
USER <andrebalazs@rocketmail.com>
2025-10-31 13:03:46 +0200

    Initial comit

---
+++ web.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import functions
+
+def add_todo():
+    todo=st.session_state['new_todo'] +'\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+todos=functions.get_todos()
+
+st.title('My todo app')
+st.subheader('this is my todo app.')
+st.write('This app is to increase your productivity')
+
+for index, todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label='Enter a todo to add it:',
+              placeholder='add a todo...', on_change=add_todo,key='new_todo')
